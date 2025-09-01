d9f62e561bce90bb8e526d6ffd39386c42bd965f
asifhodq11 <asifhodq11@gmail.com>
2025-09-01 08:39:09 +0530

    Create taskRoutes.js

---
+++ routes/taskRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/taskController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Apply the 'protect' middleware to all routes in this file
+router.use(protect);
+
+router.route('/').get(getTasks).post(createTask);
+router.route('/:id').put(updateTask).delete(deleteTask);
+
+module.exports = router;
